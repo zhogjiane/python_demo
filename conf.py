bbce1ef5357eced12196574321a4dca7311e9c74
李永东 <liyongdong@hoteamsoft.com>
2021-12-15 17:31:00 +0800

    “12-15update”

--- conf.py
+++ conf.py
@@ -1,23 +1,22 @@
 # 配置信息
+@staticmethod
 class Config:
     # 基本配置
-    SECRET_KEY = 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # 邮件配置
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 25
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = '
-    MAIL_PASSWORD = '
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <
-    FLASKY_ADMIN = '
-    # 分页配置
-    FLASKY_POSTS_PER_PAGE = 20
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
+
     # 图片上传配置
     UPLOAD_FOLDER = 'app/static/uploads'
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-    
+
+   # minio 配置
+    MINIO_ACCESS_KEY = 'minioaccesskey'
+    MINIO_SECRET_KEY = 'miniosecretkey'
+    MINIO_ENDPOINT = 'http://localhost:9000'
+    MINIO_BUCKET_NAME = 'minio-bucket'
+    MINIO_BUCKET_DOMAIN = 'http://localhost:9000'
+    MINIO_BUCKET_REGION = 'us-east-1'
+    MINIO_BUCKET_ACL = 'public-read'
+    MINIO_BUCKET_CANNED_ACL = 'public-read'
+    MINIO_BUCKET_ENCRYPTION = 'AES256'
+    MINIO_BUCKET_ENCRYPTION_KEY = 'minioencryptionkey'
